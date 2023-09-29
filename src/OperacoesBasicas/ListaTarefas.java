39f84484d7a73a8532b340e43757daadb346b536
DanielMarcos1 <daniel04pessoal@gmail.com>
2023-09-29 15:17:05 -0300

    Setup

---
+++ src/OperacoesBasicas/ListaTarefas.java
@@ -0,0 +1,19 @@
+package OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+    
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefas() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+    
+    
+}
